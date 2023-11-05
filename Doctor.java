ba22c787e69f30dfdf7fb4dfa4c18d8952ce8afc
DalbertSun1 <143521340+DalbertSun1@users.noreply.github.com>
2023-11-05 16:15:48 -0500

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,17 +1,46 @@
 import java.util.ArrayList;
 
 public class Doctor {
-    private ArrayList<Appointment> appointments ;
+    private String name; // Doctor's name
+    private ArrayList<Appointment> appointments; // List of appointments for the doctor
 
-    public Doctor(ArrayList<Appointment> appointments) {
-        this.appointments = appointments;
+    public Doctor(String name) {
+        this.name = name;
+        this.appointments = new ArrayList<>();
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public ArrayList<Appointment> getAppointments() {
         return appointments;
     }
 
-    public void setAppointments(ArrayList<Appointment> appointments) {
-        this.appointments = appointments;
+    public void addAppointment(Appointment appointment) {
+        appointments.add(appointment);
     }
 
+    public void removeAppointment(Appointment appointment) {
+        appointments.remove(appointment);
+    }
+
+    public boolean isAvailable(Time time) {
+        // Check if the doctor has an available appointment slot at the specified time
+        for (Appointment appointment : appointments) {
+            if (appointment.getTime().equals(time) && !appointment.isBooked()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
