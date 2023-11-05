39ddf13b6f77505523156d1a93c0989a890ee9a5
DalbertSun1 <143521340+DalbertSun1@users.noreply.github.com>
2023-11-05 16:10:56 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,2 +1,28 @@
 public class User {
+    public static void main(String[] args) {
+        
+//     login
+//			-if customer
+//				-make/request apppointment
+//					-view calendar
+//						-select day (shows doctors)
+//							-select doctor
+//								-make appointment
+//									-select time
+//							    -view availability
+//				-view appointments
+//					-remove appointment
+//					
+//						
+//			-if dentaloffice
+//				-view current approved appointments made (calendar)
+//					-view name and time per day
+//              -add doctor
+//				-view customer appointment requests
+//                  -approve appointment
+//                  -decline appointment    
+        
+        
+        
+    }
 }
