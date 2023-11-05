dcf0637223ba1a05c0ab8a705dba5f1858985592
DalbertSun1 <143521340+DalbertSun1@users.noreply.github.com>
2023-11-05 16:16:03 -0500

    Update Day.java

--- Day.java
+++ Day.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+
 public class Day {
+    private String date; // Date of the day
+    private ArrayList<Doctor> doctors; // List of doctors available on the day
+
+    public Day(String date) {
+        this.date = date;
+        this.doctors = new ArrayList<>();
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public ArrayList<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public void addDoctor(Doctor doctor) {
+        doctors.add(doctor);
+    }
+
+    public void removeDoctor(Doctor doctor) {
+        doctors.remove(doctor);
+    }
+
+    @Override
+    public String toString() {
+        return date;
+    }
 }
