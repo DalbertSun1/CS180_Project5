2c53b2e75d12557b8a25ca02468944165fa5cb5e
jackthetoga <white942@purdue.edu>
2023-11-08 15:13:08 -0500

    implemented DentistOffice, added getStatistics() to Doctor

--- DentistOffice.java
+++ DentistOffice.java
@@ -1,2 +1,33 @@
+/**
+ *  A DentistOffice is our equivalent of a seller. It holds Doctors, each of which has a room
+ *  contains the following methods: Add, Delete, getStatistics, Reschedule
+ * 
+ */
+ import java.util.ArrayList;
 public class DentistOffice {
+    private String name;
+    private ArrayList<Doctor> DoctorList = new ArrayList<>();
+    public DentistOffice(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String Add(Doctor doctor) { // add a Doctor to this office
+        // returns a success message, otherwise throws exception
+        DoctorList.add(doctor);
+        return "Succesfully added Doctor " + doctor.getName() + " to " + this.name;
+    }
+    public String Delete(Doctor doctor) { // remove a Doctor from this office
+        // returns a success message, otherwise throws exception
+        DoctorList.remove(doctor);
+        return "Succesfully removed Doctor " + doctor.getName() + " from " + this.name;
+    }
+    public String getStatistics() {
+        return "";
+    }
+    
+
 }
--- Doctor.java
+++ Doctor.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Doctor {
+public class Doctor { // a doctor is equivalent to a store in the project handout
     private String name; // Doctor's name
     private ArrayList<Appointment> appointments; // List of appointments for the doctor
+
 
     public Doctor(String name) {
         this.name = name;
@@ -48,6 +50,36 @@ public class Doctor {
             }
         }
         return null;
+    }
+
+    public HashMap[] getStatistics() { 
+        // returns an array of two hashmaps. 
+        // Map 1: a list of customers and corresponding # of appointments per customer
+        // Map 2: a list of Times and corresponding # of appointments
+       
+        HashMap<String, Integer> customerData = new HashMap<String, Integer>(); // maps customer names to integer # of appointments
+        HashMap<Time, Integer> timeData = new HashMap<Time, Integer>(); // maps Times to frequency of appointment slot
+
+        for (Appointment apt : appointments) {
+            String cusName = apt.getCustomerName();
+            Time thisTime = apt.getTime();
+            if (apt.isBooked()) {
+                if (customerData.containsKey(cusName)) { // if the customer is already in database, increase by 1
+                    customerData.replace(cusName, customerData.get(cusName) + 1);
+                } else { // add customer to database
+                    customerData.put(cusName, 1);
+                }
+                if (timeData.containsKey(thisTime)) { // if the time is already in database, increase by 1
+                    timeData.replace(thisTime, timeData.get(thisTime) + 1);
+                } else { // add time to database
+                    timeData.put(thisTime, 1);
+                }
+                
+            }
+        }
+        HashMap[] output = {customerData, timeData};
+        return output; 
+
     }
 
     @Override
