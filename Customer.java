b319f16d96b46e3b77d0c4c30dffcd5b357a097e
DalbertSun1 <143521340+DalbertSun1@users.noreply.github.com>
2023-11-10 10:41:01 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,14 +1,44 @@
+import java.util.ArrayList;
+
 public class Customer {
-    private String name;
-    
-    
-    //view calendar 
-    //make/request appointments
-    //remove appointments
-    //change appointments
-    
+    private String name; // Customer's name
+    private ArrayList<Appointment> appointments; // List of customer's appointments
+
+    public Customer(String name) {
+        this.name = name;
+        this.appointments = new ArrayList<>();
+    }
+
     public String getName() {
         return name;
     }
-    
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void addAppointment(Appointment appointment) {
+        appointments.add(appointment);
+    }
+
+    public void removeAppointment(Appointment appointment) {
+        appointments.remove(appointment);
+    }
+
+    public String viewAppointments() {
+        StringBuilder appointmentInfo = new StringBuilder("Appointments for " + name + ":\n");
+        for (Appointment appointment : appointments) {
+            appointmentInfo.append(appointment.toString()).append("\n");
+        }
+        return appointmentInfo.toString();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
