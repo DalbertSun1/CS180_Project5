a9b464621875877beba06f28dda648e6398c6106
jackthetoga <white942@purdue.edu>
2023-11-11 11:01:34 -0500

    created CalendarFile.java

--- CalendarFile.java
+++ CalendarFile.java
@@ -1,21 +1,67 @@
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
+
+import java.io.BufferedWriter;
 import java.io.FileWriter;
-public class CalendarFromFile {
+import java.io.IOException;
+import java.io.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+public class CalendarFile {
     // a customer calls this class, which will convert the given calendar into a CSV file
     // conversely, a CSV file can be converted into a calendar
-    public static void writeCalendar(MyCalendar calender, String filepath) {
-        try (FileWriter writer = new FileWriter(new File(filepath));
-        CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT)) {
-            csvPrinter.printRecord(person.getName(), person.getAge(), person.getEmail());
+
+    // a calendar holds days, which holds doctors, which holds appointments
+    public static void writeCalendar(MyCalendar calendar, String filepath) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(new File(filepath)))) {
+            int i = 0;
+            writer.println("dayIndex, date, startDoctor, doctorName, startApt, time, isAvailable, isBooked, customerName, endApt, endDoctor, ");
+            for (Day thisDay : calendar.getDays()) {
+                // each Day marked with a new line
+                StringBuilder line = new StringBuilder();
+                line.append(i + "," + thisDay.getDate() + ",");
+                i++;
+                for (Doctor doctor : thisDay.getDoctors()) {
+                    line.append("startDoctor,");
+                    line.append(doctor.getName() + ",");
+                    for (Appointment apt : doctor.getAppointments()) {
+                        line.append("startApt,");
+                        // add time
+                        line.append(apt.getTime().toString() + "," + apt.getTime().isAvailable() + ",");
+                        if (apt.isBooked()) {
+                            // add isBooked, add customerName
+                            line.append("true," + apt.getCustomerName() + ",");
+                        } else {
+                            line.append("false,null,");
+                        }
+
+                        line.append("endApt,");
+                    }
+                    line.append("endDoctor,");
+                }
+                writer.println(line);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
     public static MyCalendar readCalendar(String filepath) {
+        ArrayList<Day> days = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(filepath)))) {
+            String line;
+            // iterate through days
+            while ((line = reader.readLine()) != null) { // while there are days left
+                //"dayIndex, date, startDoctor, doctorName, startApt, time, isAvailable, isBooked, customerName, endApt, endDoctor,;
+                ArrayList<String> values = new ArrayList<>(Arrays.asList(line.split(",")));
+                Day thisDay = new Day(values.get(1)); // initialized day with String date
 
+
+                days.add(thisDay);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 }
