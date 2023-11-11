551aa44b07524f999479fc36fb4bfb12c5b0a164
Aaryan Bondre <143767560+aaryanbondre@users.noreply.github.com>
2023-11-11 09:46:06 -0500

    fixed name issues in login.java

--- Login.java
+++ Login.java
@@ -7,19 +7,20 @@ import java.io.FileReader;
 import java.util.*;
 
 public class Login {
-    private String name;
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the Dentist's Office!");
-
+        String fullName = "";
         boolean menu1 = false; //counter to rerun the loop if invalid choice is entered
         do {
             System.out.println("Menu\n1. Log in\n2. Create an account");
             int login = scan.nextInt();
             scan.nextLine();
+            
 
             switch (login) {
+            	
                 case 1 : // log in
                     System.out.println("Log in as\n1. Patient\n2. Doctor");
                     int identity2 = scan.nextInt();
@@ -28,14 +29,14 @@ public class Login {
                     String username2 = scan.nextLine();
                     System.out.println("Enter your password:");
                     String password2 = scan.nextLine();
-                    login(identity2, username2, password2);
+                    login(fullName, identity2, username2, password2, scan);
                     break;
                 case 2 : // create an account
                     System.out.println("Create an account as\n1. Patient\n2. Doctor");
                     int identity = scan.nextInt();
                     scan.nextLine();
                     System.out.println("Enter your full name:");
-                    name = scan.nextLine();
+                    fullName = scan.nextLine();
                     System.out.println("Enter a user name:");
                     String username = scan.nextLine();
                     System.out.println("Enter a password:");
@@ -44,7 +45,7 @@ public class Login {
                     String email = scan.nextLine();
                     System.out.println("Enter your phone number:");
                     String phoneNumber = scan.nextLine();
-                    createAccount(identity, fullName, username, password, email, phoneNumber);
+                    createAccount(identity, fullName, username, password, email, phoneNumber, scan);
                     break;
                 default : // invalid choice
                     System.out.println("Please enter a valid choice.");
@@ -55,7 +56,7 @@ public class Login {
 
     }
 
-    public static void login(int identity, String username, String password) {
+    public static void login(String fullName, int identity, String username, String password, Scanner scan) {
         if (checkAccount(username, password)) {
             System.out.println("Welcome!");
             // continue as a doctor or patient
@@ -63,7 +64,7 @@ public class Login {
             do {
                 switch (identity) {
                     case 1:
-                        Patient p = new Patient(name); // continue as a patient
+                        Patient p = new Patient(fullName); // continue as a patient
                         p.go(scan);
                         break;
                     case 2:
@@ -83,7 +84,7 @@ public class Login {
     //creates a new account
     //prints error if account already exists - to do
     public static void createAccount(int identity, String fullName, String username,
-                                     String password, String email, String phoneNumber) {
+                                     String password, String email, String phoneNumber, Scanner scan) {
         try {
             File f = new File("accounts.txt"); //creates accounts file
             FileOutputStream fos = new FileOutputStream(f, true);
@@ -97,7 +98,7 @@ public class Login {
                 System.out.println("Account successfully created!");
             }
             pw.close();
-            login(identity, username, password);
+            login(fullName, identity, username, password, scan);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -135,3 +136,4 @@ public class Login {
     }
 
 }
+
