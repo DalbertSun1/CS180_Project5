a8dcb2895fb16f01cf0610776c69cf3f7537eaa7
jackthetoga <143345085+jackthetoga@users.noreply.github.com>
2023-11-12 17:15:54 -0500

    Update Login.java

--- Login.java
+++ Login.java
@@ -111,7 +111,7 @@ public class Login {
                 switch (identity) {
                     case 1:
                         Patient p = new Patient(fullName); // continue as a patient
-                        p.go(scan, readDoctorList);
+                        p.go(scan, readDoctorList, d);
                         break;
                     case 2:
                         // continue as a doctor
