edffe7ef616f4cfa0d191c78893be689eae50721
DalbertSun1 <143521340+DalbertSun1@users.noreply.github.com>
2023-11-13 18:14:45 -0500

    final updates

--- Day.java
+++ Day.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 
+/**
+ * Project 4
+ * Dentist Office Calendar Marketplace
+ *
+ * @author Dalbert Sun, Vihaan Chadha, Jack White, Himaja Narajala, Aaryan Bondre
+ * @version November 13th, 2023
+ */
+
 public class Day {
     private int date; // Date of the day
     private ArrayList<Doctor> doctors; // List of doctors available on the day
@@ -12,15 +20,17 @@ public class Day {
     public int getDate() {
         return date;
     }
+
     public int getIndividualDoctorIndex(String doctor) {
-    	int j = -1;
-    	for (int i = 0; i < doctors.size(); i++) {
-    		if (doctors.get(i).getName().equals(doctor)) {
-    			return i;
-    		}
-    	}
-    	return j;
+        int j = -1;
+        for (int i = 0; i < doctors.size(); i++) {
+            if (doctors.get(i).getName().equals(doctor)) {
+                return i;
+            }
+        }
+        return j;
     }
+
     public void setDate(int date) {
         this.date = date;
     }
@@ -44,36 +54,38 @@ public class Day {
     public Doctor getIndividualDoctor(int index) {
         return doctors.get(index);
     }
-    public ArrayList<String> listAppts() {
-    	
-    	ArrayList<String> list = new ArrayList<String>();
-    	
-    	for (int i = 0; i < doctors.size(); i++) {
-    		for (int j = 0; j < doctors.get(i).getAppointments().size(); j++) {
-    			String a = doctors.get(i).getAppointments().get(j).getTime();
-    			String d = "";
-    			if (doctors.get(i).getName().length() > 9) {
-    				d+= doctors.get(i).getName().substring(0,9) + ": " + a.substring(0, a.indexOf('-')-1);
-    			} else {
-    				String b = doctors.get(i).getName() + ": " + a.substring(0, a.indexOf('-')-1);
-    				if (b.length() < 19) {
-    					d+= b;
-    					for (int k = 0; k < 19-b.length(); k++) {
-    						d += " ";
-    					}
-    				} else {
 
-        				d+= doctors.get(i).getName() + ": " + a.substring(0, a.indexOf('-')-1);
-    				}
-    			}
-    			
-    			list.add(d);
-    			
-    		}
-    	}
-    	
-    	return list;
+    public ArrayList<String> listAppts() {
+
+        ArrayList<String> list = new ArrayList<String>();
+
+        for (int i = 0; i < doctors.size(); i++) {
+            for (int j = 0; j < doctors.get(i).getAppointments().size(); j++) {
+                String a = doctors.get(i).getAppointments().get(j).getTime();
+                String d = "";
+                if (doctors.get(i).getName().length() > 9) {
+                    d += doctors.get(i).getName().substring(0, 9) + ": " + a.substring(0, a.indexOf('-') - 1);
+                } else {
+                    String b = doctors.get(i).getName() + ": " + a.substring(0, a.indexOf('-') - 1);
+                    if (b.length() < 19) {
+                        d += b;
+                        for (int k = 0; k < 19 - b.length(); k++) {
+                            d += " ";
+                        }
+                    } else {
+
+                        d += doctors.get(i).getName() + ": " + a.substring(0, a.indexOf('-') - 1);
+                    }
+                }
+
+                list.add(d);
+
+            }
+        }
+
+        return list;
     }
+
     public String showDoctorList() {
         String result = "";
 
